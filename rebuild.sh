bc5dc875ee04c89e8df2b334cee2ea652591127d
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-11-12 01:23:41 +0100

    Build: logs when building containers

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,8 @@
-bash remove.sh && bash build.sh && bash run.sh
+echo "Removing old container..."
+bash remove.sh 
+echo "Building new container..."
+bash build.sh
+echo "Starting up new container..."
+bash run.sh
+
+echo "New instance running!"
